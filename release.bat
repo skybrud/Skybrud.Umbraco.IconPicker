1af339cf56360a3b5b6e4fcd4d5c8b586d09a1c4
Anders Bjerner <abjerner@skybrud.dk>
2019-06-18 23:08:52 +0200

    New release :tada:

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
 @echo off
 cd src/Skybrud.Umbraco.IconPicker
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /t:rebuild /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
 cd ../../
